9b56660ad79509371d6b07d397efe5cdef2786f0
AnasuyaGhosh123 <anasuyabela@gmail.com>
2025-10-04 21:59:25 +0000

    categorization code

---
+++ categorize_summarize.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from transformers import pipeline
+from tqdm import tqdm
+import requests
+from bs4 import BeautifulSoup
+
+dataset = pd.read_csv("SB_publication_PMC.csv", header=None)
+df.columns = ["Title", "Link"]
+
+categories = {
+    "Altered Gravity": "",
+    "Radiation": "",
+    "ISS": "",
+    "Mars": "",
+    "Moon": "",
+    "Musculoskeletal": "",
+    "Cardiovascular & Vascular": "",
+    "Neuroscience & Sensory": "",
+    "Immune Response & Infection": "",
+    "Metabolism & Cellular Stress": "",
+    "Reproduction & Sex Differences": "",
+    "Humans": "",
+    "Rodents": "",
+    "Invertebrates": "",
+    "Plants": "",
+    "Microorganism and Viruses": ""
+}
+
+labels = list(categories.keys())
+
+#device = 0 if torch.cuda.is_available() else -1  
+# Load zero-shot classification pipeline
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli", device=-1)
+
+def categorize(text):
+    result = classifier(text[:1000], labels)
+    return result["labels"][0]
+
+# Add results to dataset
+df['Category'] = df['Text'].apply(categorize)
+# Save new dataset
+df.to_csv("data/papers_categorized.csv", index=False)
+
+print("\n✅ Categorization complete!")
+print(f"Results saved to 'SB_publication_PMC_categorized.csv'")
