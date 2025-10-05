4cf21fa8ab13f7cd72ab919fb0f52d14b6ac7557
AnasuyaGhosh123 <anasuyabela@gmail.com>
2025-10-05 04:33:44 +0000

    categorization code - Anasuya

--- categorize_summarize.py
+++ categorize_summarize.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import requests
 from bs4 import BeautifulSoup
 
-dataset = pd.read_csv("SB_publication_PMC.csv", header=None)
+df = pd.read_csv("data/SB_publication_PMC.csv", header=None)
 df.columns = ["Title", "Link"]
 
 categories = {
@@ -27,18 +27,22 @@ categories = {
     "Microorganism and Viruses": ""
 }
 
+
+
 labels = list(categories.keys())
 
 #device = 0 if torch.cuda.is_available() else -1  
 # Load zero-shot classification pipeline
 classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli", device=-1)
 
+
 def categorize(text):
     result = classifier(text[:1000], labels)
     return result["labels"][0]
 
+print("check")
 # Add results to dataset
-df['Category'] = df['Text'].apply(categorize)
+print(df['Title'].apply(categorize))
 # Save new dataset
 df.to_csv("data/papers_categorized.csv", index=False)
 
