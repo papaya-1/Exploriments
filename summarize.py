fc1abaa7b60052b6ad6ceb2ef54dd572fec61561
papaya_1 <mayapkumanan@gmail.com>
2025-10-05 16:58:48 +0000

    summary works, but badly

--- summarize.py
+++ summarize.py
@@ -6,7 +6,7 @@ import re
 from transformers import pipeline
 
 # loading extracted data, quote char for any commas in text so it isn't interpreted as a column, on_bad_lines to skip any lines causing issues
-df = pd.read_csv("data/extracted_papers.csv", engine = "python", quotechar = '"', on_bad_lines = 'skip')
+df = pd.read_csv("data/extracted_papers.csv", engine = "python", quotechar = '"', on_bad_lines = 'skip', skipinitialspace = True)
 
 # loads summarizer model 
 summarizer = pipeline("summarization", model = "facebook/bart-large-cnn")
@@ -23,7 +23,7 @@ def extract_abstract(entire_text):
 
     if match: 
         #returns solely the abstract
-        return match.group(1).strip 
+        return match.group(1).strip()
     else:
         #if the section can't be found, takes the first 300 words since that is the typical length of an abstract
         return text[:300]; 
@@ -31,12 +31,12 @@ def extract_abstract(entire_text):
 def summarize(text):
 
     #reduces text if it is too long so hugging face can process 
-    text = text[:1000];
+    #text = text[:1000];
 
     #running summary model 
-    summary = summarizer(text, max_length=150, min_length = 50, do_sample = False)
+    summary = summarizer(text, max_length=300, min_length = 50, do_sample = False)
 
-    return summary
+    return summary[0]["summary_text"]
 
 #extract abstract for every paper 
 for i, row in df.iterrows():
@@ -69,4 +69,3 @@ csv = "summaries.csv";
 pd.DataFrame(summaries).to_csv(csv, index = False)
 
 print("Summaries finished")
-
